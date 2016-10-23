562d98af5bee0d42d138d624fc72b9d43219329e
bcornelius <bencornelius@Bens-MacBook-Pro.local>
2016-10-23 19:32:32 -0400

    Timer
    Made a Timer

---
+++ Assets/Scripts/Countdown_Timer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Countdown_Timer : MonoBehaviour {
+
+	float timeRemaining = 30;
+	public Text countText;
+	private int count;
+
+	void Start () {
+
+	}
+
+	void Update () {
+		timeRemaining -= Time.deltaTime;
+		if (count >= 12)
+			timeRemaining = timeRemaining;
+	}
+
+	void OnGUI(){
+		if (timeRemaining > 0) {
+			GUI.Label (new Rect (450, 10, 200, 300), "Time Remaining : " + (int)timeRemaining);
+
+
+		} else {
+			if (GUI.Button(new Rect(59, 10, 150, 100), "Restart Level"))
+			SceneManager.LoadScene ("Mini_Game");
+			}
+		}
+
+		}
+
+
+
+
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,21 @@
+﻿/*
+ *Roll-A-Ball Tutorial from Unity3D.com 
+ */
+
+//This is goingto be a modification
+
+using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+	}
+}
